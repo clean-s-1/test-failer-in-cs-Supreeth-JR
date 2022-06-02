3425a7df16f1d75d152821a1ef9aee22fb9b1649
Supreeth JR <65713127+Supreeth-JR@users.noreply.github.com>
2022-06-02 14:33:30 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -1,13 +1,20 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 
 namespace AlerterSpace {
     class Alerter {
         static int alertFailureCount = 0;
+        static int highTemparatureCount = 0;
         static int networkAlertStub(float celcius) {
             Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
             // Return 200 for ok
             // Return 500 for not-ok
             // stub always succeeds and returns 200
+            if (celcius > 37)
+            {
+                highTemparatureCount++;
+            }
+
             return 200;
         }
         static void alertInCelcius(float farenheit) {
@@ -26,6 +33,7 @@ namespace AlerterSpace {
             alertInCelcius(303.6f);
             Console.WriteLine("{0} alerts failed.", alertFailureCount);
             Console.WriteLine("All is well (maybe!)\n");
+            Debug.Assert(alertFailureCount == highTemparatureCount);
         }
     }
 }
