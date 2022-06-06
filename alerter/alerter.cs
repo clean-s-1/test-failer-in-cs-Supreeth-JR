0201a3d2ad96fbf58e0f1f8470761b4c5c3ecdef
Supreeth JR <65713127+Supreeth-JR@users.noreply.github.com>
2022-06-06 15:18:13 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -1,39 +1,53 @@
 using System;
 using System.Diagnostics;
 
-namespace AlerterSpace {
-    class Alerter {
+namespace AlerterSpace
+{
+    class Alerter
+    {
         static int alertFailureCount = 0;
-        static int highTemparatureCount = 0;
-        static int networkAlertStub(float celcius) {
-            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
-            // Return 200 for ok
-            // Return 500 for not-ok
-            // stub always succeeds and returns 200
+        static int networkAlertStub(float celcius)
+        {
+            PrintOutput(celcius);
             if (celcius > 37)
             {
-                highTemparatureCount++;
+                return 500;
             }
-
             return 200;
         }
-        static void alertInCelcius(float farenheit) {
-            float celcius = (farenheit - 32) * 5 / 9;
+        static void alertInCelcius(float farenheit)
+        {
+            float celcius = ConvertToCelcius(farenheit);
             int returnCode = networkAlertStub(celcius);
-            if (returnCode != 200) {
-                // non-ok response is not an error! Issues happen in life!
-                // let us keep a count of failures to report
-                // However, this code doesn't count failures!
-                // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;
+            if (returnCode != 200)
+            {
+                alertFailureCount += 1;
             }
         }
-        static void Main(string[] args) {
+        static float ConvertToCelcius(float farenheit)
+        {
+            float celcius = (farenheit - 32) * 5 / 9;
+            return celcius;
+        }
+
+        static void PrintOutput(float celcius)
+        {
+            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
+        }
+        static void Main(string[] args)
+        {
             alertInCelcius(400.5f);
             alertInCelcius(303.6f);
-            Console.WriteLine("{0} alerts failed.", alertFailureCount);
-            Console.WriteLine("All is well (maybe!)\n");
-            Debug.Assert(alertFailureCount == highTemparatureCount);
+            alertInCelcius(95);
+            Debug.Assert(alertFailureCount == 2);
+
+            int networkAlertStubResult = networkAlertStub(37);
+            Debug.Assert(networkAlertStubResult == 200);
+            networkAlertStubResult = networkAlertStub(38);
+            Debug.Assert(networkAlertStubResult == 500);
+
+            double clecius = ConvertToCelcius(303.6f);
+            Debug.Assert(Math.Round(clecius,1)  == 150.9);
         }
     }
 }
