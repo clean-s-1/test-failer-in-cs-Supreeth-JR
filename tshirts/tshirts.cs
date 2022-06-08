9fecee68b770804a582c240d772a893c5c7b4358
Supreeth JR <65713127+Supreeth-JR@users.noreply.github.com>
2022-06-08 19:26:32 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -1,22 +1,31 @@
-﻿using System;
 using System.Diagnostics;
 
-namespace TshirtSpace {
-    class Tshirt {
-        static string Size(int cms) {
-            if(cms < 38) {
+namespace TshirtSpace
+{
+    class Tshirt
+    {
+        static string Size(int cms)
+        {
+            if (cms <= 38)
+            {
                 return "S";
-            } else if(cms > 38 && cms < 42) {
+            }
+            else if (cms >= 39 && cms <= 42)
+            {
                 return "M";
-            } else {
+            }
+            else
+            {
                 return "L";
             }
         }
-        static void Main(string[] args) {
+        private
+        static void Main(string[] args)
+        {
             Debug.Assert(Size(37) == "S");
             Debug.Assert(Size(40) == "M");
             Debug.Assert(Size(43) == "L");
-            Debug.Assert(Size(38) == "M");
+            Debug.Assert(Size(38) == "S");
         }
     }
 }
